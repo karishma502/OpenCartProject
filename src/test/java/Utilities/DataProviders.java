f015bd45a1f38bfe52acc9294969fbef670cb1db
Karishma <karishmakate31@gmail.com>
2025-04-09 22:24:01 +0530

    #Excellutility

--- src/test/java/Utilities/DataProviders.java
+++ src/test/java/Utilities/DataProviders.java
@@ -15,10 +15,10 @@ public class DataProviders {
         int tolalRow= utils.getRowCount("Sheet1");
         int totalCol= utils.getCellCount("Sheet1",1);
 
-        String loginData[][] = new String[tolalRow][totalCol]; //created for 2 dimention array
-        for(int i=1;i<tolalRow;i++){ //1  //read data from excel and storing it at i
+        String[][] loginData = new String[tolalRow][totalCol]; //created for 2 dimention array
+        for(int i=1;i<=tolalRow;i++){ //1  //read data from excel and storing it at i
             for (int j=0;j<totalCol;j++){ //0  i is row and j is col
-              loginData[i-1][j]= utils.getCellData("Sheet1",i,j); //1,0
+                 loginData[i-1][j]= utils.getCellData("Sheet1",i,j); //1,0
             }
         }
 
